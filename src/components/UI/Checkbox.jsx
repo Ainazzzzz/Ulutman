e6b821bb7858046ef7fbdc6f8cc28bd1603bb1ab
Aidana Turdueva <ajdanaturdueva9@gmail.com>
2024-06-24 09:14:18 +0300

    added label

--- src/components/UI/Checkbox.jsx
+++ src/components/UI/Checkbox.jsx
@@ -1,45 +1,51 @@
-import { Checkbox } from '@mui/material';
+import { Checkbox, FormControlLabel } from '@mui/material';
 
-export const CheckBox = ({ onChange, checked, ...props }) => {
+export const CheckBox = ({ label, onChange, checked, ...props }) => {
    return (
-      <Checkbox
-         type="checkbox"
-         onChange={onChange}
-         checked={checked}
-         {...props}
-         icon={
-            <svg width="20" height="20" viewBox="0 0 24 24">
-               <rect
-                  x="3"
-                  y="3"
-                  width="18"
-                  height="18"
-                  stroke="rgba(0, 0, 0, 0.6)"
-                  strokeWidth="1"
-                  fill="none"
-               />
-            </svg>
+      <FormControlLabel
+         control={
+            <Checkbox
+               type="checkbox"
+               label={label}
+               onChange={onChange}
+               checked={checked}
+               {...props}
+               icon={
+                  <svg width="20" height="20" viewBox="0 0 24 24">
+                     <rect
+                        x="3"
+                        y="3"
+                        width="18"
+                        height="18"
+                        stroke="rgba(0, 0, 0, 0.6)"
+                        strokeWidth="1"
+                        fill="none"
+                     />
+                  </svg>
+               }
+               checkedIcon={
+                  <svg width="20" height="20" viewBox="0 0 24 24">
+                     <rect
+                        x="3"
+                        y="3"
+                        width="18"
+                        height="18"
+                        stroke="rgba(0, 0, 0, 0.6)"
+                        strokeWidth="1"
+                        fill="none"
+                     />
+                     <path
+                        d="M5 13l4 4L19 7"
+                        stroke="#282828"
+                        strokeWidth="1"
+                        fill="none"
+                        strokeLinejoin="miter"
+                     />
+                  </svg>
+               }
+            />
          }
-         checkedIcon={
-            <svg width="20" height="20" viewBox="0 0 24 24">
-               <rect
-                  x="3"
-                  y="3"
-                  width="18"
-                  height="18"
-                  stroke="rgba(0, 0, 0, 0.6)"
-                  strokeWidth="1"
-                  fill="none"
-               />
-               <path
-                  d="M5 13l4 4L19 7"
-                  stroke="#282828"
-                  strokeWidth="1"
-                  fill="none"
-                  strokeLinejoin="miter"
-               />
-            </svg>
-         }
+         label={label}
       />
    );
 };
