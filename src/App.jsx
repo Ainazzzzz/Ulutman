b682c4cb28d120a31ff04931486a1472c7eb580f
Aiposha <aipow7269@gmail.com>
2024-06-24 19:24:14 +0600

    create footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Footer from './components/UI/Footer';
+
 function App() {
    return <div>Ulutman</div>;
 }
---
+++ src/components/UI/Footer.jsx
@@ -0,0 +1,47 @@
+import { styled } from '@mui/material';
+import React from 'react';
+
+const Footer = () => {
+   return (
+      <WrapperDiv>
+         <TitleUlutman>Ulutman</TitleUlutman>
+         <ContainerCategory>
+            <div>Работа</div>
+            <div>Аренда</div>
+            <div>Гостиница</div>
+            <div>Недвижимость</div>
+            <div>Услуги</div>
+            <div>Авто</div>
+            <div>Продам</div>
+         </ContainerCategory>
+      </WrapperDiv>
+   );
+};
+
+export default Footer;
+const WrapperDiv = styled('div')(() => ({
+   display: 'flex',
+   justifyContent: 'center',
+   alignItems: 'center',
+   width: '100%',
+   gap: '80px',
+   height: '118px',
+   background: 'rgb(218, 219, 224)',
+}));
+const ContainerCategory = styled('div')(() => ({
+   display: 'flex',
+   gap: '18px',
+   color: 'rgb(40, 40, 40)',
+   fontFamily: 'Inter',
+   fontSize: '16px',
+   fontWeight: '400',
+   lineHeight: '19px',
+}));
+const TitleUlutman = styled('div')(() => ({
+   color: 'rgb(40, 40, 40)',
+   fontFamily: 'Inter',
+   fontSize: '22px',
+   fontWeight: '600',
+   lineHeight: '27px',
+   textTransform: 'uppercase',
+}));
