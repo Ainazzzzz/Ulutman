896a3ab0395f28be1350cdf0317d2ff8a1266bab
Aidana Turdueva <ajdanaturdueva9@gmail.com>
2024-06-25 07:48:53 +0300

    added icons

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -3,25 +3,51 @@ import Ulutman from '../assets/icons/ulutman.svg?react';
 import HeartLike from '../assets/icons/white-heart.svg?react';
 import UserLogo from '../assets/icons/user.svg?react';
 import RussianFlag from '../assets/icons/russian-flag.svg?react';
+import { styled } from '@mui/material';
 export const Header = () => {
    return (
       <Wrapper>
          <Ulutman />
-         <div>
-            <HeartLike />
-            <a to="/">Избранное</a>
-         </div>
-         <div>
-            <UserLogo />
-            <a to="/">Профиль</a>
-         </div>
-         <div>
-            <RussianFlag />
-            <a to="/">Русский</a>
-         </div>
+         <ContainerBlock>
+            <Block>
+               <HeartLike />
+               <a to="/">Избранное</a>
+            </Block>
+            <Block>
+               <UserLogo />
+               <a to="/">Профиль</a>
+            </Block>
+            <Block>
+               <RussianFlag
+                  style={{ paddingTop: '6px', paddingLeft: '10px' }}
+               />
+               <a to="/">Русский</a>
+            </Block>
+         </ContainerBlock>
       </Wrapper>
    );
 };
 const Wrapper = styled('div')(() => ({
+   heigh: '84px',
+   background: 'gray',
    display: 'flex',
+   alignItems: 'center',
+   justifyContent: 'space-between',
+   padding: '14px 60px ',
+   svg: {
+      cursor: 'pointer',
+   },
+}));
+const Block = styled('div')(() => ({
+   a: {
+      fontWeight: '400',
+      cursor: 'pointer',
+   },
+   display: 'flex',
+   gap: '2px',
+   alignItems: 'center',
+}));
+const ContainerBlock = styled('div')(() => ({
+   display: 'flex',
+   gap: '32px',
 }));
