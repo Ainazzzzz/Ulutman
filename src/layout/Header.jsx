d9a328461ed3ede31f422466ffaa752244dd5459
Aidana Turdueva <ajdanaturdueva9@gmail.com>
2024-06-30 12:02:44 +0300

    save

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -6,12 +6,36 @@ import Plus from '../assets/icons/plus.svg?react';
 import Menu from '../assets/icons/menu-icon.svg?react';
 import PlusButton from '../assets/icons/plus-button-icon.svg?react';
 import UlutmanLogo from '../assets/icons/ulutman-logo-icon.svg?react';
+import MessageIcon from '../assets/icons/message-icon.svg?react';
 import { styled, useMediaQuery } from '@mui/material';
 import { IconButton } from '../components/IconButton';
 import { Button } from '../components/UI/Button';
+import ReusableSelect from '../components/UI/Select';
+import { languages } from '../utils/constants/languages';
+import { useState } from 'react';
+
+const renderFlag = language =>
+   language === 'Кыргызский' ? (
+      <UserLogo />
+   ) : language === 'Русский' ? (
+      <RussianFlag />
+   ) : language === 'Турецкий' ? (
+      <TurkishFlag />
+   ) : language === 'Узбекский' ? (
+      <UzbekFlag />
+   ) : language === 'Английский' ? (
+      <EnglishFlag />
+   ) : (
+      <RussianFlag />
+   );
+
 export const Header = () => {
    const isMobile = useMediaQuery(theme => theme.breakpoints.down('md'));
+   const [language, setLanguage] = useState('Русский');
 
+   const handleSelect = event => {
+      setLanguage(event.target.value);
+   };
    return (
       <Wrapper>
          <LogoStyle>{isMobile ? <Ulutman /> : <UlutmanLogo />}</LogoStyle>
@@ -32,6 +56,12 @@ export const Header = () => {
                <ContainerBlock>
                   <Block>
                      <IconButton>
+                        <MessageIcon />
+                     </IconButton>
+                     <a>Сообщения</a>
+                  </Block>
+                  <Block>
+                     <IconButton>
                         <HeartLike />
                      </IconButton>
                      <a>Избранное</a>
@@ -43,12 +73,14 @@ export const Header = () => {
                      <a>Профиль</a>
                   </Block>
                   <Block>
-                     <IconButton>
-                        <RussianFlag
-                           style={{ paddingTop: '8px', marginRight: '-10px' }}
-                        />
-                     </IconButton>
-                     <a>Русский</a>
+                     <div style={{ paddingTop: '10px' }}>
+                        {renderFlag(language)}
+                     </div>
+                     <SelectStyle
+                        options={languages}
+                        value={language}
+                        onChange={handleSelect}
+                     />
                   </Block>
                   <ButtonStyle>
                      <Plus />
@@ -63,7 +95,7 @@ export const Header = () => {
 
 const Wrapper = styled('header')(({ theme }) => ({
    height: '84px',
-   background: 'gray',
+   background: '#ffffff',
    display: 'flex',
    alignItems: 'center',
    justifyContent: 'space-between',
@@ -106,73 +138,22 @@ const ButtonStyle = styled(Button)(() => ({
    textTransform: 'inherit',
    height: '36px',
 }));
-// const ButtonMobileStyle = styled(Button)(() => ({
-//    borderRadius: '50px',
-//    width: '27px',
-//    height: '27px',
-// }));
-// export const Header = () => {
-//    return (
-//       <Wrapper>
-//          <Ulutman />
-//          <ContainerBlock>
-//             <Block>
-//                <IconButton>
-//                   <HeartLike />
-//                </IconButton>
-//                <a to="/">Избранное</a>
-//             </Block>
-//             <Block>
-//                <IconButton>
-//                   <UserLogo />
-//                </IconButton>
-//                <a to="/">Профиль</a>
-//             </Block>
-//             <Block>
-//                <IconButton>
-//                   <RussianFlag
-//                      style={{ paddingTop: '6px', paddingLeft: '10px' }}
-//                   />
-//                </IconButton>
-//                <a to="/">Русский</a>
-//             </Block>
-//             <ButtonStyle>
-//                {' '}
-//                <Plus />
-//                Опубликовать
-//             </ButtonStyle>
-//          </ContainerBlock>
-//       </Wrapper>
-//    );
-// };
-// const Wrapper = styled('div')(() => ({
-//    heigh: '84px',
-//    background: '#FFF',
-//    display: 'flex',
-//    alignItems: 'center',
-//    justifyContent: 'space-between',
-//    padding: '14px 60px ',
-//    svg: {
-//       cursor: 'pointer',
-//    },
-// }));
-// const Block = styled('div')(() => ({
-//    a: {
-//       fontWeight: '400',
-//       cursor: 'pointer',
-//    },
-//    display: 'flex',
-//    alignItems: 'center',
-// }));
-// const ContainerBlock = styled('div')(() => ({
-//    display: 'flex',
-//    alignItems: 'center',
-//    gap: '32px',
-// }));
-// const ButtonStyle = styled(Button)(() => ({
-//    display: 'flex',
-//    gap: '4px',
-//    fontWeight: '500',
-//    textTransform: 'inherit',
-//    height: '36px',
-// }));
+const SelectStyle = styled(ReusableSelect)(() => ({
+   '.MuiOutlinedInput-notchedOutline': {
+      border: 'none',
+   },
+   '&:hover .MuiOutlinedInput-notchedOutline': {
+      border: 'none',
+   },
+   '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+      border: 'none',
+   },
+
+   '.MuiSelect-icon': {
+      right: '5px',
+      top: '15px',
+   },
+   '.MuiSelect-select': {
+      paddingLeft: '0px',
+   },
+}));
---
+++ src/utils/constants/languages.js
@@ -0,0 +1,15 @@
+export const languages = [
+   { label: 'Кыргызский', value: 'Кыргызский' },
+   { label: 'Русский', value: 'Русский' },
+   { label: 'Турецкий', value: 'Турецкий' },
+   { label: 'Узбекский', value: 'Узбекский' },
+   { label: 'Английский', value: 'Английский' },
+];
+
+const flags = {
+   Кыргызский: 'path/to/kyrgyzstan-flag.svg',
+   Русский: 'path/to/russian-flag.svg',
+   Турецкий: 'path/to/turkish-flag.svg',
+   Узбекский: 'path/to/uzbekistan-flag.svg',
+   Английский: 'path/to/english-flag.svg',
+};
