58a7aedd8a62571fe78551e5118022ddc2c28fae
Aidana Turdueva <ajdanaturdueva9@gmail.com>
2024-07-02 12:13:33 +0300

    added menu

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,18 +1,22 @@
-import Ulutman from '../assets/icons/ulutman-icon.svg?react';
 import HeartLike from '../assets/icons/white-heart.svg?react';
 import UserLogo from '../assets/icons/user.svg?react';
 import RussianFlag from '../assets/icons/russian-flag.svg?react';
 import Plus from '../assets/icons/plus.svg?react';
-import Menu from '../assets/icons/menu-icon.svg?react';
-import PlusButton from '../assets/icons/plus-button-icon.svg?react';
+import MenuIcon from '../assets/icons/menu-icon.svg?react';
 import UlutmanLogo from '../assets/icons/ulutman-logo-icon.svg?react';
 import MessageIcon from '../assets/icons/message-icon.svg?react';
+import ComeIcon from '../assets/icons/come-icon.svg?react';
+import WhiteHeart from '../assets/icons/white-heart-icon.svg?react';
+import WhiteMessage from '../assets/icons/white-message-icon.svg?react';
+import Language from '../assets/icons/language-icon.svg?react';
 import { styled, useMediaQuery } from '@mui/material';
 import { IconButton } from '../components/IconButton';
 import { Button } from '../components/UI/Button';
 import ReusableSelect from '../components/UI/Select';
 import { languages } from '../utils/constants/languages';
 import { useState } from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 const renderFlag = language =>
    language === 'Кыргызский' ? (
@@ -28,28 +32,76 @@ const renderFlag = language =>
    ) : (
       <RussianFlag />
    );
-
+const SearchIcon = ({ color = '#ffffff' }) => (
+   <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+   >
+      <path
+         d="M20.9998 21L15.8028 15.803M15.8028 15.803C17.2094 14.3965 17.9996 12.4887 17.9996 10.4995C17.9996 8.51035 17.2094 6.60262 15.8028 5.19605C14.3962 3.78947 12.4885 2.99927 10.4993 2.99927C8.51011 2.99927 6.60238 3.78947 5.19581 5.19605C3.78923 6.60262 2.99902 8.51035 2.99902 10.4995C2.99902 12.4887 3.78923 14.3965 5.19581 15.803C6.60238 17.2096 8.51011 17.9998 10.4993 17.9998C12.4885 17.9998 14.3962 17.2096 15.8028 15.803Z"
+         stroke={color}
+         strokeWidth="1.5"
+         strokeLinecap="round"
+         strokeLinejoin="round"
+      />
+   </svg>
+);
 export const Header = () => {
    const isMobile = useMediaQuery(theme => theme.breakpoints.down('md'));
    const [language, setLanguage] = useState('Русский');
+   const [openMenu, setOpenMenu] = useState(null);
 
    const handleSelect = event => {
       setLanguage(event.target.value);
    };
+   const handleClick = event => {
+      setOpenMenu(event.currentTarget);
+   };
+   const handleClose = () => {
+      setOpenMenu(null);
+   };
    return (
       <Wrapper>
-         <LogoStyle>{isMobile ? <Ulutman /> : <UlutmanLogo />}</LogoStyle>
+         <LogoStyle>
+            <UlutmanLogo />
+         </LogoStyle>
          {isMobile ? (
             <div>
-               <IconButton>
-                  <HeartLike />
+               <IconButton onClick={handleClick}>
+                  <MenuIcon />
                </IconButton>
-               <IconButton>
-                  <PlusButton />
-               </IconButton>
-               <IconButton>
-                  <Menu />
-               </IconButton>
+               <MenuStyle
+                  anchorEl={openMenu}
+                  open={Boolean(openMenu)}
+                  onClose={handleClose}
+               >
+                  <MenuItemStyle onClick={handleClose}>
+                     <ComeIcon /> Войти
+                  </MenuItemStyle>
+                  <Line></Line>
+                  <MenuItemStyle onClick={handleClose}>
+                     <SearchIcon color="#fff" />
+                     Поиск
+                  </MenuItemStyle>
+                  <MenuItemStyle onClick={handleClose}>
+                     <Plus /> Опубликовать
+                  </MenuItemStyle>
+                  <MenuItemStyle onClick={handleClose}>
+                     <WhiteHeart />
+                     Избранное
+                  </MenuItemStyle>
+                  <MenuItemStyle onClick={handleClose}>
+                     <WhiteMessage />
+                     Сообщения
+                  </MenuItemStyle>
+                  <MenuItemStyle onClick={handleClose}>
+                     <Language />
+                     Сменить язык
+                  </MenuItemStyle>
+               </MenuStyle>
             </div>
          ) : (
             <>
@@ -124,10 +176,16 @@ const ContainerBlock = styled('div')(() => ({
    alignItems: 'center',
    gap: '32px',
 }));
-const LogoStyle = styled('div')(() => ({
+const LogoStyle = styled('div')(({ theme }) => ({
    display: 'flex',
    alignItems: 'center',
    gap: '10px',
+   svg: {
+      [theme.breakpoints.down('md')]: {
+         width: '134px',
+         height: '29px',
+      },
+   },
 }));
 
 const ButtonStyle = styled(Button)(() => ({
@@ -156,4 +214,35 @@ const SelectStyle = styled(ReusableSelect)(() => ({
    '.MuiSelect-select': {
       paddingLeft: '0px',
    },
+}));
+
+const MenuStyle = styled(Menu)(() => ({
+   '.MuiPaper-root': {
+      padding: '16px 0px 16px 0px',
+      width: '230px',
+      background: '#7e52ff',
+   },
+}));
+const MenuItemStyle = styled(MenuItem)(() => ({
+   display: 'flex',
+   gap: '10px',
+   paddingLeft: '40px',
+   color: '#fff',
+   fontWeight: '600',
+   '&:hover': {
+      backgroundColor: '#fff',
+      color: '#7e52ff',
+      borderTopRightRadius: '8px',
+      borderBottomRightRadius: '8px',
+      marginRight: '16px',
+      '& svg path': {
+         stroke: '#7e51ff',
+      },
+   },
+}));
+
+const Line = styled('div')(() => ({
+   width: '100%',
+   borderBottom: '1px solid #b2b2b2',
+   margin: '16px 0px 16px 0px',
 }));
