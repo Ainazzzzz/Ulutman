f6c2838eb3d11d947499d9e639b9c7603e664209
Sheralieva Bermet <bermetsheralieva79@gmail.com>
2024-07-03 16:31:50 +0600

    clear App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,5 @@
-import Modal from './components/UI/Modal';
-
 function App() {
-   const open = true;
-   return (
-      <div>
-         Ulutma n
-         <Modal open={open}>
-            bermet
-            <button>lkasdf</button>
-            <button>lkasdfkjfasd</button>
-         </Modal>
-      </div>
-   );
+   return <div>Ulutman</div>;
 }
 
 export default App;
