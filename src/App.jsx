e93f5267dc8367015cd028ead0cf1179f7a6c87c
Aidana Turdueva <ajdanaturdueva9@gmail.com>
2024-07-18 14:46:09 +0300

    created ads

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import { Ads } from './components/Admin/Ads';
+
 function App() {
-   return <div>Ulutman</div>;
+   return (
+      <div>
+         Ulutman
+         <Ads />
+      </div>
+   );
 }
 
 export default App;
