443f865ff28bad5e155a72cc5479efa8c897cb73
Aiposha <aipow7269@gmail.com>
2024-07-18 23:51:09 +0300

    createcheckbox

--- src/components/Admin/AddAdmin.jsx
+++ src/components/Admin/AddAdmin.jsx
@@ -8,9 +8,11 @@ import RadioButton from '../UI/RadioButton';
 
 const AddAdmin = () => {
    const [selectedOption, setSelectedOption] = useState('');
+
    const handleOptionChange = event => {
       setSelectedOption(event.target.value);
    };
+
    return (
       <div>
          <ContainerTitleArrow>
@@ -22,7 +24,9 @@ const AddAdmin = () => {
          </ContainerTitleArrow>
          <SehondBigContainer>
             <InputStyle>
-               <Search />
+               <SearchIconStyle>
+                  <Search />
+               </SearchIconStyle>
                <InputBase placeholder="Поиск" />
             </InputStyle>
             <RadioGroup>
@@ -63,28 +67,46 @@ const ContainerTitleArrow = styled('div')(() => ({
    display: 'flex',
    justifyContent: 'space-around',
 }));
-const ArrowBox = styled('div')(() => ({
+const ArrowBox = styled('div')(({ theme }) => ({
    display: 'flex',
+   paddingTop: '20px',
+   [theme.breakpoints.down('md')]: {
+      display: 'none',
+   },
 }));
-const SehondBigContainer = styled('div')(() => ({
+const SehondBigContainer = styled('div')(({ theme }) => ({
    paddingLeft: '260px',
+   [theme.breakpoints.down('md')]: {
+      paddingLeft: '30px',
+   },
 }));
 
-const TitleSyle = styled('div')(() => ({
+const TitleSyle = styled('div')(({ theme }) => ({
    color: 'rgb(40, 40, 40)',
    fontFamily: 'Kanit',
    fontSize: '34px',
    fontWeight: '600',
    lineHeight: '51px',
    paddingBottom: '40px',
+   [theme.breakpoints.down('md')]: {
+      fontSize: '27px',
+      fontWeight: '600',
+      lineHeight: '36px',
+      paddingBottom: '24px',
+   },
 }));
 
 const InputStyle = styled('div')(() => ({
-   width: '338px',
+   display: 'flex',
+   gap: '15px',
+   width: '343px',
    height: '38px',
    border: '0.6px solid rgb(213, 213, 213)',
    borderRadius: '19px',
    background: 'rgb(245, 246, 250)',
+}));
+const SearchIconStyle = styled('div')(() => ({
+   padding: '10px 0 10px 10px',
 }));
 const RadioButtonStyle = styled('div')(() => ({
    display: 'flex',
@@ -104,12 +126,18 @@ const CheckboxStyle = styled('div')(() => ({
    gap: '35px',
    paddingBottom: '35px',
 }));
-const CheckboxFive = styled(CheckBox)(() => ({
+const CheckboxFive = styled(CheckBox)(({ theme }) => ({
    color: 'rgb(0, 0, 0)',
    fontFamily: 'Inter',
    fontSize: '14px',
    fontWeight: '800',
    lineHeight: '17px',
+   [theme.breakpoints.down('md')]: {
+      lineHeight: '16px',
+      fontSize: '14px',
+      fontWeight: '500',
+      height: '17px',
+   },
 }));
 const BackStyle = styled('p')(() => ({
    color: 'rgb(126, 82, 255)',
