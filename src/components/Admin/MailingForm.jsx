ba3d4b33d63e34e14c8da6f479292bde31b791ee
Nooruz <abdykalykovnooruz638@gmail.com>
2024-07-22 22:01:18 +0300

    craete mailing form

---
+++ src/components/Admin/MailingForm.jsx
@@ -0,0 +1,191 @@
+import { useFormik } from 'formik';
+import ReusableSelect from '../UI/Select';
+import Input from '../UI/Input';
+
+import { validationSchema } from '../../utils/constants/validationMailing';
+import {
+   WrapperInputSelect,
+   InputFile,
+   CameraIcon,
+   Container,
+   Label,
+   StyledButton,
+   StyledWriting,
+   ErrorMessage,
+   DownloadTitle,
+} from './MailingFormStyles';
+
+export const MailingForm = ({ mailingType, recipients }) => {
+   const formik = useFormik({
+      initialValues: {
+         mailings: '',
+         typeMailing: 'Новости',
+         writing: '',
+         recipientsAllValue: 'Все пользователи',
+         files: null,
+      },
+      validationSchema: validationSchema,
+      onSubmit: values => {
+         console.log(values);
+      },
+   });
+
+   return (
+      <form onSubmit={formik.handleSubmit}>
+         <WrapperInputSelect>
+            <InputContainer
+               name="mailings"
+               value={formik.values.mailings}
+               onChange={formik.handleChange}
+               onBlur={formik.handleBlur}
+               touched={formik.touched.mailings}
+               errors={formik.errors.mailings}
+               placeholder="Новости платформы"
+               label="Название рассылки"
+            />
+
+            <SelectContainer
+               name="typeMailing"
+               value={formik.values.typeMailing}
+               onChange={formik.handleChange}
+               onBlur={formik.handleBlur}
+               touched={formik.touched.typeMailing}
+               errors={formik.errors.typeMailing}
+               label="Тип рассылки"
+               options={mailingType}
+            />
+
+            <SelectContainer
+               name="recipientsAllValue"
+               value={formik.values.recipientsAllValue}
+               onChange={formik.handleChange}
+               onBlur={formik.handleBlur}
+               touched={formik.touched.recipientsAllValue}
+               errors={formik.errors.recipientsAllValue}
+               label="Получатели"
+               options={recipients}
+            />
+
+            <TextAreaContainer
+               name="writing"
+               value={formik.values.writing}
+               onChange={formik.handleChange}
+               onBlur={formik.handleBlur}
+               touched={formik.touched.writing}
+               errors={formik.errors.writing}
+               placeholder="Горячие акции: Скидка 20% на премиум-размещение: Разместите ваше объявление в топе и привлеките больше внимания! Предложение действует до [Дата]."
+               label="Описание рассылки"
+            />
+         </WrapperInputSelect>
+
+         <DownloadTitle>Загрузите фото</DownloadTitle>
+
+         <FileUpload
+            setFieldValue={formik.setFieldValue}
+            touched={formik.touched.files}
+            errors={formik.errors.files}
+         />
+
+         <StyledButton type="submit">Отправить</StyledButton>
+      </form>
+   );
+};
+
+const InputContainer = ({
+   name,
+   value,
+   onChange,
+   onBlur,
+   touched,
+   errors,
+   placeholder,
+   label,
+}) => (
+   <Container>
+      <Input
+         name={name}
+         value={value}
+         onChange={onChange}
+         onBlur={onBlur}
+         placeholder={placeholder}
+         label={label}
+         error={touched && Boolean(errors)}
+      />
+      {touched && errors ? <ErrorMessage>{errors}</ErrorMessage> : null}
+   </Container>
+);
+
+const SelectContainer = ({
+   name,
+   value,
+   onChange,
+   onBlur,
+   touched,
+   errors,
+   label,
+   options,
+}) => (
+   <Container>
+      <ReusableSelect
+         name={name}
+         value={value}
+         label={label}
+         options={options}
+         onChange={onChange}
+         onBlur={onBlur}
+         error={touched && Boolean(errors)}
+      />
+      {touched && errors ? <ErrorMessage>{errors}</ErrorMessage> : null}
+   </Container>
+);
+
+const TextAreaContainer = ({
+   name,
+   value,
+   onChange,
+   onBlur,
+   touched,
+   errors,
+   placeholder,
+   label,
+}) => (
+   <Container>
+      <label htmlFor={name}>{label}</label>
+      <StyledWriting
+         name={name}
+         id={name}
+         value={value}
+         onChange={onChange}
+         onBlur={onBlur}
+         placeholder={placeholder}
+         error={touched && Boolean(errors)}
+      />
+      {touched && errors ? <ErrorMessage>{errors}</ErrorMessage> : null}
+   </Container>
+);
+
+const FileUpload = ({ setFieldValue, touched, errors }) => (
+   <Label htmlFor="raised-button-file">
+      <Container>
+         <InputFile
+            accept="image/*"
+            id="raised-button-file"
+            multiple
+            type="file"
+            name="files"
+            onChange={event =>
+               setFieldValue('files', event.currentTarget.files[0])
+            }
+            onBlur={setFieldValue.handleBlur}
+         />
+         <div className="container-error">
+            {touched && errors ? (
+               <ErrorMessage className="files">{errors}</ErrorMessage>
+            ) : null}
+         </div>
+      </Container>
+      <p>Для добавления картинки щелкните или перетащите его</p>
+      <b>Добавьте фото</b>
+      <CameraIcon />
+   </Label>
+);
---
+++ src/components/Admin/MailingFormStyles.jsx
@@ -0,0 +1,123 @@
+import { styled } from '@mui/material';
+import CameraAltIcon from '../../assets/icons/camera-icon.svg?react';
+import { Button } from '../UI/Button';
+
+export const WrapperInputSelect = styled('div')(({ theme }) => ({
+   width: '327px',
+   display: 'flex',
+   flexDirection: 'column',
+   gap: '24px',
+   padding: '24px 0 20px 0',
+   [theme.breakpoints.down('md')]: {
+      flexDirection: 'column',
+      gap: '15px',
+   },
+   '.MuiFormControl-root': {
+      gap: '0',
+   },
+   label: {
+      fontSize: '18px',
+      fontWeight: '600',
+      marginBottom: '8px',
+   },
+}));
+
+export const InputFile = styled('input')({
+   display: 'none',
+});
+
+export const CameraIcon = styled(CameraAltIcon)({
+   fontSize: 48,
+   color: '#777',
+});
+
+export const Container = styled('div')(({ theme }) => ({
+   width: '100%',
+   position: 'relative',
+   [theme.breakpoints.down('md')]: {
+      '.MuiInputBase-root': {
+         width: '100%',
+      },
+   },
+   '.container-error': {
+      display: 'flex',
+      justifyContent: 'center',
+   },
+}));
+
+export const Label = styled('label')(() => ({
+   display: 'flex',
+   alignItems: 'center',
+   justifyContent: 'center',
+   flexDirection: 'column-reverse',
+   gap: '15px',
+   width: '255px',
+   height: '199px',
+   borderRadius: '4px',
+   cursor: 'pointer',
+   background: '#7E52FF1A',
+   marginBottom: '30px',
+   b: {
+      color: '#282828',
+      fontWeight: '600',
+   },
+   p: {
+      width: '215px',
+      textAlign: 'center',
+      color: '#909090',
+      fontSize: '14px',
+      fontWeight: '400',
+   },
+}));
+
+export const StyledButton = styled(Button)(({ theme }) => ({
+   width: '144px',
+   [theme.breakpoints.down('md')]: {
+      width: '100%',
+      maxWidth: '343px',
+   },
+}));
+
+export const StyledWriting = styled('textarea')(({ theme }) => ({
+   width: '617px',
+   height: '122px',
+   fontSize: '18px',
+   fontWeight: '400',
+   color: '#909090',
+   padding: '10px',
+   border: '1px solid #CFCFCF',
+   borderRadius: '10px',
+   background: 'transparent',
+   resize: 'none',
+   marginTop: '8px',
+   [theme.breakpoints.down('md')]: {
+      width: '326px',
+      height: '154px',
+      '::placeholder': {
+         fontSize: '18px',
+         fontWeight: '400',
+      },
+   },
+   '&:hover': {
+      border: '1px solid #282828',
+   },
+   '&:focus': {
+      border: '1px solid #7e52ff',
+   },
+}));
+
+export const ErrorMessage = styled('div')(() => ({
+   color: 'red',
+   fontSize: '14px',
+   fontWeight: '300',
+   position: 'absolute',
+}));
+
+export const DownloadTitle = styled('p')(({ theme }) => ({
+   fontSize: '18px !important',
+   fontWeight: '600',
+   marginBottom: '8px',
+   [theme.breakpoints.down('md')]: {
+      fontSize: '14px',
+   },
+}));
