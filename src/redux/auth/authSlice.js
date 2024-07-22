2d68c352b58cb16507d4123df97760bae2b98d1a
Nooruz <abdykalykovnooruz638@gmail.com>
2024-07-22 22:32:45 +0300

    change ADMIN to GUEST

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -22,7 +22,7 @@ const getInitialState = () => {
       lastName: '',
       email: '',
       token: '',
-      role: 'ADMIN',
+      role: 'GUEST',
       isAuth: false,
       isLoading: false,
       error: null,
