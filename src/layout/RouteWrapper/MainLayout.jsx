72a37d64df4552fea409665f6565af1c21b690da
AziatAbd <abdimalikovaziat@gmail.com>
2024-07-24 02:34:16 +0600

    delete pagination from mainLayout

--- src/layout/RouteWrapper/MainLayout.jsx
+++ src/layout/RouteWrapper/MainLayout.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import CustomPagination from '../../components/UI/Pagination';
 
 export const MainLayout = () => {
    return (
       <div>
          <h1>MainLayout</h1>
-         <CustomPagination />
          <Outlet />
       </div>
    );
