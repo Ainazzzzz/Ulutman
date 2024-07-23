481bbc7163303e1f13b81ba602ed76e68930be5a
AziatAbd <abdimalikovaziat@gmail.com>
2024-07-24 02:31:48 +0600

    finished pagination

--- src/components/UI/Pagination.jsx
+++ src/components/UI/Pagination.jsx
@@ -1,29 +1,76 @@
-import React, { useState } from 'react';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
+import React, { useEffect, useState } from 'react';
+import Arrow from '../../assets/icons/slider-arrow.svg?react';
+import { Box, IconButton, styled, Typography } from '@mui/material';
 
-const CustomPagination = () => {
-   const [page, setPage] = useState(1);
-   const totalItems = 78;
-   const itemsPerPage = 9;
+const Pagination = ({ page, setPage, rowsPerPage, totalItems }) => {
+   const [prevBtnDisabled, setPrevBtnDisabled] = useState(page === 1);
+   const [nextBtnDisabled, setNextBtnDisabled] = useState(
+      page === Math.ceil(totalItems / rowsPerPage),
+   );
 
-   const handleChange = (event, value) => {
-      setPage(value);
+   const nextPage = () => {
+      if (page < Math.ceil(totalItems / rowsPerPage)) {
+         setPage(prevState => prevState + 1);
+      }
    };
 
+   const prevPage = () => {
+      if (page > 1) {
+         setPage(prevState => prevState - 1);
+      }
+   };
+
+   useEffect(() => {
+      setPrevBtnDisabled(page === 1);
+      setNextBtnDisabled(page === Math.ceil(totalItems / rowsPerPage));
+   }, [page, totalItems, rowsPerPage]);
+
    return (
-      <Stack spacing={2}>
-         <Pagination
-            count={Math.ceil(totalItems / itemsPerPage)}
-            page={page}
-            onChange={handleChange}
-            shape="rounded"
-         />
-         <div>
-            {`${(page - 1) * itemsPerPage + 1}-${Math.min(page * itemsPerPage, totalItems)} из ${totalItems}`}
-         </div>
-      </Stack>
+      <StyledStack>
+         <StyledPagination>
+            {`${(page - 1) * rowsPerPage + 1}-${Math.min(page * rowsPerPage, totalItems)} из ${totalItems}`}
+         </StyledPagination>
+         <Box>
+            <PrevArrow onClick={prevPage} disabled={prevBtnDisabled}>
+               <Arrow />
+            </PrevArrow>
+            <NextArrow onClick={nextPage} disabled={nextBtnDisabled}>
+               <Arrow />
+            </NextArrow>
+         </Box>
+      </StyledStack>
    );
 };
 
-export default CustomPagination;
+export default Pagination;
+
+const StyledStack = styled(Box)(() => ({
+   display: 'flex',
+   alignItems: 'center',
+   gap: '10px',
+}));
+
+const StyledPagination = styled(Typography)(() => ({
+   color: '#282828',
+   width: '120px',
+}));
+
+const PrevArrow = styled(IconButton)(({ theme, disabled }) => ({
+   padding: '9px 17px',
+   border: '1px solid',
+   borderRight: 'none',
+   borderRadius: '10px 0 0 10px',
+   backgroundColor: '#fff',
+   cursor: disabled ? 'not-allowed' : 'pointer',
+   opacity: disabled ? 0.5 : 1,
+}));
+
+const NextArrow = styled(IconButton)(({ theme, disabled }) => ({
+   transform: 'rotate(180deg)',
+   padding: '9px 17px',
+   border: '1px solid',
+   borderRadius: '10px 0 0 10px',
+   backgroundColor: '#fff',
+   cursor: disabled ? 'not-allowed' : 'pointer',
+   opacity: disabled ? 0.5 : 1,
+}));
