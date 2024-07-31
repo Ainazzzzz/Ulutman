64b783ec0ab89fcad616e12dac075bd5348a9624
Aidana Turdueva <ajdanaturdueva9@gmail.com>
2024-07-31 16:05:55 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import { FeaturedAds } from './components/User/FeaturedAds';
 import { AppRoutes } from './routes/AppRoutes';
 
 const App = () => {
    return (
       <>
          <AppRoutes />
-         <FeaturedAds />
       </>
    );
 };
