3addf563b3c3e86d4c5798a47bc91db6cd638d9a
Nooruz <abdykalykovnooruz638@gmail.com>
2024-08-05 16:11:07 +0300

    save

---
+++ src/pages/Admin/ModerationPage.jsx
@@ -0,0 +1,9 @@
+import { Moderation } from '../../components/Admin/Moderation';
+
+export const ModerationPage = () => {
+   return (
+      <div>
+         <Moderation />
+      </div>
+   );
+};
--- src/routes/AdminRoutes.jsx
+++ src/routes/AdminRoutes.jsx
@@ -1,4 +1,5 @@
 import { AddMailingPage } from '../pages/Admin/AddMailingPage';
+import { ModerationPage } from '../pages/Admin/ModerationPage';
 import { PrivateAuthRouteByRole } from './private/PrivateAuthRouteByRole';
 
 export const AdminRoutes = role => {
@@ -11,6 +12,17 @@ export const AdminRoutes = role => {
                roles={['ADMIN']}
                fallBackPath="/"
                RouteComponent={<AddMailingPage />}
+            />
+         ),
+      },
+      {
+         path: 'moderation',
+         element: (
+            <PrivateAuthRouteByRole
+               role={role}
+               roles={['ADMIN']}
+               fallBackPath="/"
+               RouteComponent={<ModerationPage />}
             />
          ),
       },
