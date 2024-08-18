f07da6c03fcdea5206f73f35cb7355f9d5ab0c44
Aidana Turdueva <ajdanaturdueva9@gmail.com>
2024-08-18 21:12:34 +0300

    created user-ads

--- src/components/User/Ads.jsx
+++ src/components/User/Ads.jsx
@@ -3,24 +3,43 @@ import ChevronLeft from '../../assets/icons/chevron-left.svg?react';
 import DeleteAll from '../../assets/icons/delete-all-icon.svg?react';
 import DeleteMobile from '../../assets/icons/delete-mobile-icon.svg?react';
 import { MyAds } from './MyAds';
+import TabsUi from '../UI/TabsUi';
 
 export const Ads = () => {
    const isMobile = useMediaQuery(theme => theme.breakpoints.down('md'));
+
+   const firstTab = [
+      { value: '1', label: 'Профиль' },
+      { value: '2', label: 'Избранное' },
+      { value: '3', label: 'Сообщения' },
+      { value: '4', label: 'Мои объявления' },
+   ];
+
+   const secondTab = [
+      { value: '1', label: 'Активно' },
+      { value: '2', label: 'На модерации' },
+      { value: '3', label: 'Деактивировано' },
+      { value: '4', label: 'Отклонено' },
+   ];
 
    return (
       <Wrapper>
          <Container>
             <Block>
-               <p>таблица бар</p>
+               <TabsUi tabs={firstTab} />
                <NextStyle>
                   <ChevronLeft /> Назад
                </NextStyle>
             </Block>
             <Line></Line>
             <Block>
-               <p>экинчи таблица бар </p>
+               <TabsUi tabs={secondTab} />
 
-               {isMobile ? <DeleteMobile /> : <DeleteAll />}
+               {isMobile ? (
+                  <DeleteMobile />
+               ) : (
+                  <DeleteAll style={{ marginTop: '10px' }} />
+               )}
             </Block>
          </Container>
 
@@ -28,13 +47,17 @@ export const Ads = () => {
       </Wrapper>
    );
 };
-const NextStyle = styled('span')(() => ({
+const NextStyle = styled('span')(({ theme }) => ({
    fontSize: '14px',
    fontWeight: '400',
    color: '#7e52ff',
    display: 'flex',
-   alignItems: 'center',
    gap: '4px',
+   cursor: 'pointer',
+   paddingTop: '20px',
+   [theme.breakpoints.down('md')]: {
+      paddingTop: '0px',
+   },
 }));
 
 const Line = styled('div')(() => ({
@@ -54,7 +77,6 @@ const Wrapper = styled('div')(({ theme }) => ({
 }));
 const Block = styled('div')(({ theme }) => ({
    display: 'flex',
-   //    alignItems: 'center',
    justifyContent: 'space-between',
    [theme.breakpoints.down('md')]: {
       flexDirection: 'column',
