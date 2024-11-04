cb91b02e487e70276b6c85e79161447f79f1dc98
Aidana Turdueva <ajdanaturdueva9@gmail.com>
2024-11-04 13:33:11 +0300

    delete icon

--- src/components/UI/Card/CardItem.jsx
+++ src/components/UI/Card/CardItem.jsx
@@ -2,7 +2,6 @@ import { Card, CardContent, CardMedia, styled } from '@mui/material';
 import HomeIcon from '../../../assets/icons/home-icon.svg?react';
 import AddressIcon from '../../../assets/icons/address-icon.svg?react';
 import PhoneIcon from '../../../assets/icons/phone-icon.svg?react';
-import PhoneIcon from '../../../assets/icons/phone-icon.svg?react';
 import LikeIcon from '../../../assets/icons/like-icon.svg?react';
 import { useState } from 'react';
 import { PhoneModal } from '../PhoneModal';
