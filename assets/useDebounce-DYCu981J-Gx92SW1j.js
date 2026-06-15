488419a5d0ca598226bf9a516de24dba3f32115c
Ainazik <ainazik@gmail.com>
2026-06-16 00:49:42 +0600

    Updates

---
+++ assets/useDebounce-DYCu981J-Gx92SW1j.js
@@ -0,0 +1,1 @@
+import{p as r}from"./index-goaj7iYt.js";const c=(t,e)=>{const[o,s]=r.useState(t);return r.useEffect(()=>{const u=setTimeout(()=>{s(t)},e);return()=>{clearTimeout(u)}},[t,e]),o};export{c as n};
